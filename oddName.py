76c658cabee5e6ab83ce930f88aee6722c47109a
Minh Le <jc474652@ad.jcu.edu.au>
2018-11-20 10:30:10 +0800

    Initial commit

---
+++ oddName.py
@@ -0,0 +1,8 @@
+name=input("Enter name: = ")
+
+while len(name)<=0:
+    print("Name is blank, please reenter the name")
+    name=input("Enter name: = ")
+print(name[::2])
+for i in range(0,len(name),2):
+    print(name[i],end='')
